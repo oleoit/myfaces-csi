047fd4ca32f125c689f531050d89b37c7665038e
paulrivera22 <paulrivera22@ea1d4837-9632-0410-a0b9-156113df8070>
2008-11-25 08:09:28 +0000

    Made changes to make styles cache dir more flexible.

--- trunk/testleo/myfaces-skins2/generic-support/src/main/java/org/apache/myfaces/custom/skin/webapp/StyleSheetLoader.java
+++ trunk/testleo/myfaces-skins2/generic-support/src/main/java/org/apache/myfaces/custom/skin/webapp/StyleSheetLoader.java
@@ -2,12 +2,12 @@ package org.apache.myfaces.custom.skin.webapp;
 
 import java.io.IOException;
 
-import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface StyleSheetLoader
 {
-    public void loadStyleSheet(HttpServletRequest req, HttpServletResponse resp, ServletConfig config)
+    public void loadStyleSheet(HttpServletRequest req, HttpServletResponse resp, ServletContext context)
             throws IOException;
 }
